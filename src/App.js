db813270bc86ff7941106506416e68c58d4bb36d
mkowusujr <mrfriiday@gmail.com>
2023-02-25 13:51:52 -0500

    Removed header and updated routes

--- src/App.js
+++ src/App.js
@@ -1,21 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
-import { BrowserRouter, Routes, Route, redirect } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { CreateEventPage } from './CreateEvent';
 import { ViewEventPage } from './ViewEvent';
-import { NoPage } from './NoPage';
 
 function App() {
   return (
     <BrowserRouter>
-      <div className="header">
-        <h1>Need a Ride?</h1>
-      </div>
-
       <Routes>
         <Route path="ride" element={<ViewEventPage />} />
-        <Route index element={<CreateEventPage />} />
-        <Route path="*" element={<NoPage />} />
+        <Route index path="*" element={<CreateEventPage />} />
       </Routes>
     </BrowserRouter>
   );
