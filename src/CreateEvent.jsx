52a6339cce1d35318cc79432a90f5168899e1490
Devin Vander Stelt <devin@vstelt.com>
2023-02-25 14:25:58 -0500

    Add CreateEvent page w/out api

--- src/CreateEvent.jsx
+++ src/CreateEvent.jsx
@@ -1,15 +1,36 @@
 import "./CreateEvent.css";
 
-function createEvent(name, location, time) {
-    
-
-    // send to api with fetch
-}
+import {useState} from 'react';
 
 export function CreateEventPage(props) {
+    const [name, setName] = useState("");
+    const [error, setError] = useState("");
+
+    const handleChange = (evt) => {
+        setName(evt.target.value);
+    }
+
+    const validateName = (name) => {
+        // If name is falsy, ie an empty string, undefined, or null, this evaluates to false
+        return name;
+    }
+
+    const createEvent = () => {
+        if (!validateName(name)) {
+            setError("Event Name cannot be empty");
+        }
+        //TODO: actually call create event
+    }
+
     return (
         <div>
-            <p>Hello the from Create Event Page</p>
+            <h1>Create a New Event</h1>
+            <input type="text" placeholder="Event Name" value={name} onChange={handleChange}/>
+
+            <br />
+            <input type="submit" value="Create" onClick={createEvent}/>
+            <p class="error">{error}</p>
         </div>
     )
 }
+
