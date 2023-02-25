815ad4a1dd52c25b4ef0403f6291a2999bb21e46
mkowusujr <mrfriiday@gmail.com>
2023-02-25 15:08:54 -0500

    Baisc structure for view event page

--- src/ViewEvent.jsx
+++ src/ViewEvent.jsx
@@ -1,14 +1,108 @@
+import { useState, useEffect } from "react";
 import { useSearchParams } from "react-router-dom";
 import "./CreateEvent.css";
+import { getEvent } from "./api-utils";
+
+const dummyEvent = {
+    id: 2023,
+    cars: [
+        {
+        capcity: 6,
+        riders: ["Mat", "Nic", "Dev"],
+        driver: "Justin",
+        },
+        {
+        capcity: 4,
+        riders: ["Andrew", "Nate"],
+        driver: "Jaron",
+        },
+    ],
+    unassigned: ["Logan", "Bill"],
+};
+
+const UserRegister = (props) => {
+
+};
+
+const Car = (props) => {
+    let car = props.car;
+
+    return (
+        <>
+            <div>
+                <h3>Driver</h3>
+                <p>{car.driver}</p>
+            </div>
+            <div>
+                <h4>Passengers</h4>
+                <ul>
+                    {car.riders.map(rider =>
+                        <li>{rider}</li>)}
+                </ul>
+            </div>
+            <div>
+                <button value="Join Car"></button>
+            </div>
+        </>
+    )
+}
+
+const DisplayEevnt = (props) => {
+    let event = props.event;
+
+    return (
+        <>
+            <h1>Available Cars</h1>
+            <ul>
+                {event.cars.map(car =>
+                    <li>
+                        <Car car={car} />
+                    </li>
+                )}
+            </ul>
+        </>
+    )
+};
 
 export function ViewEventPage(props) {
+    const [event, setEvent] = useState();
     const [searchParams] = useSearchParams();
-    let rideId = searchParams.get("r")
-    
-    return (
-        <div>
-            <p>Hello the from View Event Page</p>
-            <p>You are viewing the ride {rideId}</p>
-        </div>
-    )
+    let eventIdParam = searchParams.get("e");
+    console.log(eventIdParam);
+
+    const fetchEventFromApi = (eventId) => {
+        setEvent(dummyEvent);
+        // getEvent(eventId)
+        //     .then(fectchedEvent => setEvent(fectchedEvent))
+        //     .catch(error => console.error(error));
+    };
+
+    useEffect(() => {
+        if (eventIdParam) {
+        fetchEventFromApi(eventIdParam);
+        }
+    }, [eventIdParam]);
+
+  return (
+        <>
+            {event ? (
+                <>
+                    <div>
+                        <p>Hello the from View Event Page</p>
+                        <p>You are viewing the event {event.id}</p>
+                    </div>
+                    
+                    <div>
+                        <UserRegister />
+                    </div>
+                  
+                    <div>
+                        <DisplayEevnt event={event} />
+                    </div>
+                </>
+            ) : (
+                <p>Loading</p>
+            )}
+        </>
+    );
 }
