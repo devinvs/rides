1ac69ce346b66a6bef5a32bb10fb2834d59adf51
Devin Vander Stelt <devin@vstelt.com>
2023-02-25 20:50:53 -0500

    Parking lines

--- src/components/DisplayEvent.jsx
+++ src/components/DisplayEvent.jsx
@@ -1,14 +1,20 @@
 import { Car } from "./Car";
 import "./DisplayEvent.css";
 
+const Line = () => {
+    return <div className="parking-line"></div>
+}
+
 export function DisplayEevnt(props) {
     let event = props.event;
 
     return (
         <div className="parking-lot">
-            {event.cars.map(car =>
+            {event.cars.map((car, i) =>
                 <>
                 <Car car={car} />
+                {i===car.length-1? null: <Line />
+                }
                 </>
             )}
         </div>
